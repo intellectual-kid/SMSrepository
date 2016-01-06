75a5fe326219f73a22e5c925feec16ffebf60205
whizkid <whizkid@172.29.0.112>
2016-01-06 16:21:27 +0500

    Initial Commit of Student Management System

---
+++ SMS/src/login/sms/Login.java
@@ -0,0 +1,13 @@
+package login.sms;
+
+public class Login {
+
+	public static void main(String[] args) {
+		StudentData std = new StudentData();
+		std.display();
+		
+System.out.println("User Name");
+System.out.println("Password");
+	}
+
+}
---
+++ SMS/src/login/sms/StudentData.java
@@ -0,0 +1,12 @@
+package login.sms;
+
+public class StudentData {
+		int data [] = {0, 4, 42};
+		public void  display() 
+		{
+			System.out.println("Display student name");
+		}
+		
+		
+	
+}
