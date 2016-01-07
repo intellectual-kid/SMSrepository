8db8efb468cb1d6f5c87a93875b3dddf697e97f3
whizkid <whizkid@172.29.0.112>
2016-01-07 16:23:39 +0500

    back to traditional forloop.

--- SMS/src/login/sms/StudentData.java
+++ SMS/src/login/sms/StudentData.java
@@ -10,7 +10,7 @@ public class StudentData {
 		{
 			System.out.println("This is your Data ");
 			int sum=0;
-			for(int i:data)			
+			for(int i=0;i<data.length;i++)			
 			{
 				System.out.println("Next value:" + data[i]);
 				sum+=i;
